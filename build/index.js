47965a0be898b57be5b1d9c0f122592a7a5df9dd
RaineEdianel <raineedianel21@gmail.com>
2023-06-19 11:21:30 +0800

    commit

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("hello nodeJS");
+for (var i = 0; i < 3; i++) {
+    console.log(i);
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+console.log('hello nodeJS')
+for (let i = 0; i < 3; i++) {
+    console.log(i)
+}
